074e8895cf0bcec1baf48c09de0b9ae23d0ad335
sundarrajan <sundarrajan.natarajan@bpost.be>
2025-06-22 18:45:26 +0200

    inital development

---
+++ src/context/constants.ts
@@ -0,0 +1,22 @@
+type CurrencyType = '€'
+
+export const currency: CurrencyType = '€'
+
+export const currentYear = new Date().getFullYear()
+
+export const developedByLink = 'https://digimabble.com/'
+
+export const developedBy = 'digimabble'
+
+export const contactUs = 'digimabble@gmail.com'
+
+export const buyLink = ''
+
+export const basePath = ''
+
+export const DEFAULT_PAGE_TITLE = 'Pavimmo | AI  Admin Dashboard'
+
+// Replace the URL's value in env with your backend's URL or if you're using nextjs's API, add the server's origin URL
+export const API_BASE_PATH = ''
+
+export const colorVariants = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark', 'purple', 'pink', 'orange', 'light', 'link']
