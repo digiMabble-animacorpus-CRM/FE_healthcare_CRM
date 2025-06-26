32099afc85a99f14dd21513deb2348a0b6460106
Dan Christopher <Dan@digimabble.com>
2025-06-26 21:04:49 +0530

    logo and menu list updated

--- src/components/LogoBox.tsx
+++ src/components/LogoBox.tsx
@@ -1,5 +1,5 @@
-import logoDark from '@/assets/images/logo-dark.png'
-import logoLight from '@/assets/images/logo-light.png'
+import logoDark from '@/assets/images/logo-light2.png'
+import logoLight from '@/assets/images/logo-light2.png'
 import logoSm from '@/assets/images/logo-sm.png'
 import Image from 'next/image'
 import Link from 'next/link'
